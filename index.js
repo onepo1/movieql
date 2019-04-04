12c4b2cdf59c1492170135a4641e25250c9bbef3
onepo1 <1po@korea.com>
2019-04-04 21:58:37 +0900

    0404

---
+++ index.js
@@ -0,0 +1,10 @@
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+
+// const server = new GraphQLServer({});
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers
+    //   resolvers:resolvers
+});
+server.start(() => console.log("Graphql Server Running"));
